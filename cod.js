ea963944c318860081f4fe9276878ac307979c24
Meagan Pau <meagan.pau@gmail.com>
2016-01-04 23:32:23 -0500

    timer button

--- cod.js
+++ cod.js
@@ -18,6 +18,36 @@ function start ()
 }
 
 
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+function countdown() {
+    var thirty = 30,
+        display = document.querySelector('#time');
+    startTimer(thirty, display);
+};
+
+
+
+
+
+
+
 // function countdown( timer, minutes, seconds )
 // {
 //    var element, endTime, hours, mins, msLeft, time;
@@ -47,7 +77,5 @@ function start ()
 
 
 // }
-
-//
 
 // countdown( "timer", 0, 30 );
