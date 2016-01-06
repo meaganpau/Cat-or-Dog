1fa30f03bcd2873739a97a38c6b17692660e7fc1
Meagan Pau <meagan.pau@gmail.com>
2016-01-05 22:39:38 -0500

    timer not working on click but can work on load

--- cod.js
+++ cod.js
@@ -13,9 +13,9 @@ document.onkeydown = function(e) {
 function start()
 {   
     showPic ();   
-    countdown();
     document.getElementById("showButtons").innerHTML = "<input value='Cat' type='submit' id='cat_button' href='JavaScript:nextImage()''><input value='Dog' type='submit' id='dog_button' href='JavaScript:nextImage()''>";
     document.getElementById("startButton").innerHTML = "";
+    document.getElementById("startTime").innerHTML = "<div id='timer'></div>";
 }
 
 function showPic() 
@@ -26,63 +26,65 @@ function showPic()
 
 
 
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10)
+//         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+//         display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-function countdown() {
-    var thirty = 30,
-        display = document.querySelector('#time');
-    startTimer(thirty, display);
-};
-
-
-
-
-
-
-
-// function countdown( timer, minutes, seconds )
-// {
-//    var element, endTime, hours, mins, msLeft, time;
-
-//     function twoDigits( n )
-//     {
-//         return (n <= 9 ? "0" + n : n);
-//     }
-
-//     function updateTimer()
-//     {
-//         msLeft = endTime - (+new Date);
-//         if ( msLeft < 1000 ) {
-//             element.innerHTML = "Time's up!";
-//         } else {
-//             time = new Date( msLeft );
-//             hours = time.getUTCHours();
-//             mins = time.getUTCMinutes();
-//             element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-//             setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+//         if (--timer < 0) {
+//             timer = duration;
 //         }
-//     }
-
-//     element = document.getElementById("timer");
-//     endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-//     updateTimer();1
-
-
+//     }, 1000);
 // }
 
-// countdown( "timer", 0, 30 );
+// function countdown() {
+//     var thirty = 30,
+//         display = document.querySelector('#time');
+//     startTimer(thirty, display);
+// };
+
+
+
+
+function showTimer ()
+{
+    
+}
+
+function countdown( elementName, minutes, seconds )
+{
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "Time's Up!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
+countdown( "timer", 0, 30 );
+
+
